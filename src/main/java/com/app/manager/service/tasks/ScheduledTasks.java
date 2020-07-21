5c431c8bf9a1010462401912973f6258edba6e31
Senbonzakura <anhdungpham090@gmail.com>
2020-07-21 19:23:30 +0700

    fix database bug.

--- src/main/java/com/app/manager/service/tasks/ScheduledTasks.java
+++ src/main/java/com/app/manager/service/tasks/ScheduledTasks.java
@@ -14,7 +14,7 @@ public class ScheduledTasks {
 //    1800000
     @Scheduled(fixedRate = 1799950)
     public void pingTo() {
-        var url = "https://senbonzakura-ping-app.herokuapp.com/";
+        var url = "https://senbonzakura-ping-app.herokuapp.com";
         try {
             var result  = getStatus(url);
             System.out.println(result);
